4adf1d431e3d4b1ec6e93b368a1cf700da772a09
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2022-06-28 21:20:56 +0100

    Added ability to allow a user to logout and update user details

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,122 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 // Firebase
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile, updateEmail } from 'firebase/auth';
+import { updateDoc, doc } from 'firebase/firestore';
+import { db } from '../firebase/firebase.config';
 
 function Profile() {
-	const [user, setUser] = useState(null);
-
 	const auth = getAuth();
+	const [changeDetails, setChangeDetails] = useState(false);
+	const [formData, setformData] = useState({
+		name: auth.currentUser.displayName,
+		email: auth.currentUser.email,
+	});
 
-	useEffect(() => {
-		setUser(auth.currentUser);
-	}, []);
-	return user ? <h1>{user.displayName}</h1> : 'Not Logged In';
+	const { name, email } = formData;
+
+	const navigate = useNavigate();
+
+	const onLogout = () => {
+		auth.signOut();
+		navigate('/');
+	};
+
+	const onSubmit = async () => {
+		try {
+			if (auth.currentUser.displayName !== name) {
+				// Update display name in firebase
+				await updateProfile(auth.currentUser, {
+					displayName: name,
+				});
+
+				// Update in firestore
+				const userRef = doc(db, 'users', auth.currentUser.uid);
+				await updateDoc(userRef, {
+					name,
+				});
+
+				toast.success('Name updated!');
+			}
+		} catch (error) {
+			toast.error('Could not update profile details');
+		}
+
+		try {
+			console.log(auth.currentUser.email);
+			console.log(email);
+			if (auth.currentUser.email !== email) {
+				// Update display name in firebase
+				await updateEmail(auth.currentUser, email);
+
+				// Update in firestore
+				const userRef = doc(db, 'users', auth.currentUser.uid);
+				console.log(userRef);
+				await updateDoc(userRef, {
+					email,
+				});
+
+				toast.success('Email updated!');
+			}
+		} catch (error) {
+			console.log(error);
+			toast.error('Could not update email details');
+		}
+	};
+
+	const onChange = (e) => {
+		setformData((prevState) => ({
+			...prevState,
+			[e.target.id]: e.target.value,
+		}));
+	};
+
+	return (
+		<div className='profile'>
+			<header className='profileHeader'>
+				<p className='pageHeader'>My Profile</p>
+				<button className='logOut' type='button' onClick={onLogout}>
+					Logout
+				</button>
+			</header>
+			<main>
+				<div className='profileDetailsHeader'>
+					<p className='profileDetailsText'>Personal Details</p>
+					<p
+						className='changePersonalDetails'
+						onClick={() => {
+							changeDetails && onSubmit();
+							setChangeDetails((prevState) => !prevState);
+						}}>
+						{changeDetails ? 'done' : 'change'}
+					</p>
+				</div>
+
+				<div className='profileCard'>
+					<form>
+						<input
+							type='text'
+							id='name'
+							className={!changeDetails ? 'profileName' : 'profileNameActive'}
+							disabled={!changeDetails}
+							value={name}
+							onChange={onChange}
+						/>
+						<input
+							type='email'
+							id='email'
+							className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+							disabled={!changeDetails}
+							value={email}
+							onChange={onChange}
+						/>
+					</form>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default Profile;
